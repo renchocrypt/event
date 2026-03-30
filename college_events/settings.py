00df1d43ed8bcda00c3785d5f12c2b0468eae9e8
topcalls11-ops <topcalls11@gmail.com>
2026-03-30 22:14:09 +0530

    fix: postgresql support for railway

--- college_events/settings.py
+++ college_events/settings.py
@@ -1,8 +1,8 @@
 import os
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
-# .env load karo
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,10 +59,10 @@ TEMPLATES = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 AUTH_USER_MODEL = 'users.CustomUser'
